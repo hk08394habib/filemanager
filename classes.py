e9d316378478d9fd89310ca67f5b02fc19545692
hk08394habib <123847643+hk08394habib@users.noreply.github.com>
2023-04-15 17:55:47 +0500

    Using book's definition of trees this time

--- classes.py
+++ classes.py
@@ -1,99 +1,141 @@
-
-### This file contains the required class implementations for data structures
-
-class tree(): 
-    def __init__(self,nodes = [],edges=[]):
-        self.graph = {}
-        for node in nodes:
-            self.graph[nodes] = edges
-    def _addNode(self,node):
-        self.graph[node] = []
-    def _addEdge(self,edge):
-        self.graph[edge[0]] += (edge,) #give both edges
-        self.graph[edge[1]] += (edge[::-1][1:] + (-1 * edge[2],),) #reverse first two elements, keep weight the same but negative
-    def addChild(self,parent,child,weight):
-        self._addNode(child)
-        self._addEdge((parent,child,weight))
-    def getParent(self,node):
-        for x in self.graph[node]:
-            if x[2] < 0:
-                return x[1]
-        raise Exception("Node is parent")
-    def removeNode(self,node):
-        parent = self.getParent(node)
-        parentlist = self.graph[parent] 
-        for index in range(len(parentlist)-1): #remove child reference from parent
-            if parentlist[index][1] == node:
-                parentlist.pop(index)
-        self.graph.pop(node)
-
-    def iter(self):
-        return list(self.graph.items())
-
-    def BFS(): #To be implemented
-        pass
-    def DFS(): #To be implemented
-        pass
-    def INORDER(): #To be implemented
-        pass
+##The idea will be that we use /abc/efg/hij/kl/mno/filename.py to tell you a traversal of the directory graph from the root. Once you reach the node mno (which is a directory) we require that all children leaves will have unique names. This allows us to search (using binsearch or something) to find the Node with the name filename.py, which will return to us a Node object, which we can then use to create our position object, and which will allow for insertion, deletion, etc.
 
 
-class ht():
-    def __init__(self,size):
-        self.size = size
-        self.keys = [None] * self.size 
-        self.values = [None] * self.size
-        self.contained = 0 
+
+class Tree(): 
+    class Position:
+        def element(self): #element stored at position
+            raise NotImplementedError
+        def __eq__(self,other):
+            raise NotImplementedError
+        def __ne__(self,other):
+            return not (self == other)
     
-    def put(self,key,value):
-        if self.contained >= 2 * self.size // 3:
-           self.resize()
-        i = 0 
-        hashval = self.hashfunction(key)
-        rehashval = self.rehashfunction(hashval,i) 
-        while i < self.size:
-            rehashval = self.rehashfunction(key,i)
-            if self.keys[rehashval] == None:
-                self.keys[rehashval] = key
-                self.values[rehashval] = value
-                break
-            i += 1
-    def resize(self):
-        new_ht = ht(3 * self.size)
-        for x in range(self.size):
-          key = self.keys[x]
-          value = self.values[x]
-          if key != None:
-            new_ht.put(key,value)
 
-        self.size = new_ht.size
-        self.keys = new_ht.keys
-        self.values = new_ht.values
-        self.contained = new_ht.contained
+    #In what follows, p is a position, and e is an element, and t is a tree
 
-    def get(self,key):
-       i = 0 
-       hashval = self.hashfunction(key)
-       rehashval = self.rehashfunction(hashval,i) 
-       while i < self.size:
-          rehashval = self.rehashfunction(key,i)
-          if self.keys[rehashval] == key:
-            return self.values[rehashval] 
-          i += 1
-    def show(self):
-       print(self.keys)
-       print(self.values)       
-    def hashfunction(self,key):
-      return key % self.size
-    def rehashfunction(self,key, i):
-      return self.hashfunction(key+i)
+    #These will be the public access methods we will use for the class, we leave the updating ones to be defined in the concrete class
+
+    def root(self):
+        raise NotImplementedError
+
+    def parent(self,p):
+        raise NotImplementedError
+
+    def children(self,p):
+        raise NotImplementedError
+
+    def num_children(self,p):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def is_root(self,p):
+        return (p == self.root())
+
+    def is_leaf(self,p):
+        return (self.num_children(p) == 0)
+
+    def is_empty(self):
+        return (len(self) == 0)
+
+class DirTree(Tree):
+    class _Node:
+        __slots__ = '_element','_parent','_children'
+        def __init__(self, element, parent=None, children=None):
+            self._element = element
+            self._parent = parent
+            self._children = children
+    class Position(Tree.Position): #We need this in order to make sure everything is valid
+        def __init__(self,container,node):
+            self._container = container
+            self._node = node
+
+        def element(self):
+            return self._node._element
+
+        def __eq__(self,other):
+            return type(other) is type(self) and other._node is self._node
+    
+    def _validate(self,p): #Check if position is valid, and return node
+        if not isinstance(p,Position):
+            raise TypeError("Not position type")
+        if p._container is not self:
+            raise ValueError("Wrong container")
+        if p._node._parent is p._node: #We delete nodes this way, we're setting a convention
+            raise ValueError("Deprecated")
+
+        return p._node
+
+    def _make_position(self,node): #Check if node is valid and return position
+        return self.Position(self,node) if node is not None else None
+
+    def __init__(self):
+        self._root = None
+        self._size = 0
+    
+    def __len__(self):
+        return self._size
+
+    def root(self): #return the position of root
+        return self._make_position(self._root)
+
+    def parent(self,p): #return position of parent
+        node = self._validate(p) 
+        return self._make_position(node._parent)
+
+    def children(self,p): #allow to iterate over children
+        node = self._validate(p)
+        yield self._make_position(node._children)
+
+    def num_children(self,p):
+        count = 0
+        for c in self.children(p):
+            count += 1
+
+    def _add_root(self,e):
+        if self.root() is not None: raise ValueError("Already existing root")
+        self._size = 1
+        self._root = self._Node(e) 
+        return self._make_position(self._root)
+
+    def _add_child(self,p,e): ##TODO: implement path as a way to traverse the tree from the root node
+        node = self._validate(p)
+        self._size += 1
+        if node._children == None:
+            node._children = []
+        node._children += [self._Node(e,node)]
+        return self._make_position(node._left)
+
+    def _replace(self,p,e):
+        node = self._validate(p)
+        old = node._element
+        node._element = e
+        return old
+    
+    def _delete(self,p): ##TODO
+        node = self._validate(p)
+        self._size -= 1 ##num elements
+
+    def _attach(self,p,t):
+        node = self._validate(p)
+        if not self.is_leaf(p): raise ValueError('Position must be a leaf')
+        if not type(self) is type(t):
+            raise TypeError('Tree types must match')
+        self._size += len(t)
+        if not t.is_empty():
+            t._root._parent = node
+            t._root = None
+            t._size = 0 
 
 
 
 
 
 
-        
+
+
 
 
 
